c3e8ed7a82203703172fdc964333b2800f7518a8
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-09-10 18:48:18 -0600

    dask: update main script

--- dask/b-dask-k8s-ultimate/dask.sh
+++ dask/b-dask-k8s-ultimate/dask.sh
@@ -9,23 +9,77 @@ CLUSTER=1
 CLUSTER_NAME=b${CLUSTER}
 CONFIG_FILE=./configs/config-${CLUSTER_NAME}.yaml
 
-if [[ $1 == "install" || $1 == "i" ]]
-then
-  echo -e "Starting ${CLUSTER_NAME}"
-  helm install dask-k8s-${CLUSTER_NAME} dask/dask -f ${CONFIG_FILE}
-elif [[ $1 == "uninstall" || $1 == "u" ]]
-then
-  echo -e "Stopping ${CLUSTER_NAME}"
-  helm uninstall dask-k8s-b1
-elif [[ $1 == "start-dashboard" || $1 == "start-d" ]]
-then
-  echo -e "Starting Dashboard for ${CLUSTER_NAME}"
+
+function start_dashboard {
+  echo -e "Starting Dashboard for ${CLUSTER_NAME} on Port ${DASK_SCHEDULER_PORT} (UI: ${DASK_SCHEDULER_UI_PORT})"
   kubectl port-forward --namespace default svc/dask-k8s-${CLUSTER_NAME}-scheduler $DASK_SCHEDULER_PORT:8786 &
   kubectl port-forward --namespace default svc/dask-k8s-${CLUSTER_NAME}-scheduler $DASK_SCHEDULER_UI_PORT:80 &
-elif [[ $1 == "stop-dashboard" || $1 == "stop-d" ]]
-then
+}
+
+function stop_dashboard {
   echo -e "Stopping Dashboard for ${CLUSTER_NAME}"
-  ps ux | grep 'port-forward' | grep 'dask-k8s-${CLUSTER_NAME}' | awk '{print $2}' | xargs kill -9 $1
-else
-  echo "Invalid commmand: ${1}"
-fi
+  #kill -9 $(ps ux | grep 'port-forward' | grep 'dask-k8s-${CLUSTER_NAME}' | awk '{print $2}')
+  #killall -l $(ps ux | grep 'port-forward' | grep 'dask-k8s-${CLUSTER_NAME}')
+  ps ux | grep 'port-forward' | grep 'dask-k8s' | awk '{print $2}' | xargs kill -9 $1
+}
+
+
+function install {
+  echo -e "Starting ${CLUSTER_NAME}"
+  helm install dask-k8s-${CLUSTER_NAME} dask/dask -f ${CONFIG_FILE}
+}
+
+
+function uninstall {
+  echo -e "Stopping ${CLUSTER_NAME}"
+  helm uninstall dask-k8s-b1
+}
+
+function print_help {
+  echo "Help message"
+}
+
+#if [[ $1 == "install" || $1 == "i" ]]
+#then
+#  install
+#elif [[ $1 == "uninstall" || $1 == "u" ]]
+#then
+#  uninstall
+#elif [[ $1 == "start-dashboard" || $1 == "start-d" ]]
+#then
+#  start_dashboard
+#elif [[ $1 == "stop-dashboard" || $1 == "stop-d" ]]
+#then
+#  stop_dashboard
+#else
+#  echo "Invalid commmand: ${1}"
+#fi
+
+
+for var in "$@"; do
+  case "$var" in
+    "--install" | "-i")
+      install
+      exit 0
+      ;;
+    "--uninstall" | "-u")
+      uninstall
+      exit 0
+      ;;
+    "--start-dashboard" | "-start-d")
+      start_dashboard
+      exit 0
+      ;;
+    "--stop-dashboard" | "-stop-d")
+      stop_dashboard
+      exit 0
+      ;;
+    *)
+      echo "Unrecognized option $var"
+      print_help
+      exit 1
+      ;;
+  esac
+done
+
+
